346712a6b256d25f4dba881f4b42aba79bd4c877
Jahangir-Alam-Noman <jahangir2km@gmail>
2022-01-04 12:08:27 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyD0UvJnu3vMoNj7uFr9h_3bRk4cruitLXU",
+	authDomain: "tesla-1131e.firebaseapp.com",
+	projectId: "tesla-1131e",
+	storageBucket: "tesla-1131e.appspot.com",
+	messagingSenderId: "608238417457",
+	appId: "1:608238417457:web:d13bc3ece4607bdf238710"
+};
+
+
+export default firebaseConfig;
